0f928bf0ca622ab66829ca6c99d3c6b5770b59c9
kepeket <kprettre@graph-e.com>
2013-12-29 00:15:00 +0100

    Update CaptureOrderAction.php

--- Payum/Dummy/Action/CaptureOrderAction.php
+++ Payum/Dummy/Action/CaptureOrderAction.php
@@ -14,7 +14,7 @@ namespace Sylius\Bundle\PayumBundle\Payum\Dummy\Action;
 use Payum\Action\PaymentAwareAction;
 use Payum\Exception\RequestNotSupportedException;
 use Payum\Request\CaptureRequest;
-use Sylius\Bundle\CoreBundle\Model\OrderInterface;
+use Sylius\Bundle\OrderBundle\Model\OrderInterface;
 
 class CaptureOrderAction extends PaymentAwareAction
 {
